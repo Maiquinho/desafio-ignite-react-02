3ec3aa5cf2208ea9d4ab29bd1f4125069f07444f
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-17 15:37:14 -0300

    feat: add styled components & default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./global";
+
 export function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h2>Titulo</h2>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
---
+++ src/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+export const GlobalStyle = createGlobalStyle`
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background: ${(props) => props.theme.background};
+    }
+
+    body, input, textarea, button {
+        font-family: "Roboto", sans-serif;
+        font-size: 1rem;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        font-family: "Baloo 2", sans-serif;
+        line-height: 1.3;
+    }
+
+`
