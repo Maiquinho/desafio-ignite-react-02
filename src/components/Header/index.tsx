779109f640bbe5652c15f107e6f8e3d00852a193
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-18 17:49:33 -0300

    feat: add Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,34 @@
-export function Header(){
-    return <header>Header</header>
+import { NavLink } from 'react-router-dom'
+import { Container } from '../Container'
+import { Cart, HeaderWrapper, Location, Navigation } from './styles'
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+
+export function Header() {
+  return (
+    <Container>
+      <HeaderWrapper>
+        <NavLink to="/">
+          <img src="/logo.svg" alt="Logo Coffee Delivery" />
+        </NavLink>
+
+        <Navigation>
+          <ul>
+            <li>
+              <Location>
+                <MapPin size={22} weight="fill" />
+                <span>Porto Alegre, RS</span>
+              </Location>
+            </li>
+            <li>
+              <NavLink to="/cart">
+                <Cart>
+                  <ShoppingCart size={22} weight="fill" />
+                </Cart>
+              </NavLink>
+            </li>
+          </ul>
+        </Navigation>
+      </HeaderWrapper>
+    </Container>
+  )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const HeaderWrapper = styled.header`
+  padding: 2rem 0;
+
+  display: flex;
+  justify-content: space-between;
+`
+
+export const Navigation = styled.nav`
+  & ul {
+    display: flex;
+    gap: 0.875rem;
+
+    & li {
+      list-style-type: none;
+    }
+  }
+`
+
+export const ActionButtonBase = styled.span`
+  padding: 0.5rem;
+
+  display: flex;
+  align-items: center;
+
+  border-radius: 6px;
+`
+
+export const Location = styled(ActionButtonBase)`
+  background: ${(props) => props.theme['purple-light']};
+  gap: 0.25rem;
+
+  & span {
+    font-size: 0.875rem;
+    color: ${(props) => props.theme['purple-dark']};
+  }
+
+  & svg {
+    fill: ${(props) => props.theme.purple};
+  }
+`
+
+export const Cart = styled(ActionButtonBase)`
+  background: ${(props) => props.theme['yellow-light']};
+
+  & svg {
+    fill: ${(props) => props.theme['yellow-dark']};
+  }
+`
