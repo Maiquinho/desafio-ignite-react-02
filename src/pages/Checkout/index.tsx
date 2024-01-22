11e1740e8402caaed6bcf569e85f4fc050597e3e
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-22 10:31:03 -0300

    feat: add Checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,219 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPin,
+  Minus,
+  Money,
+  Plus,
+  Trash,
+} from '@phosphor-icons/react'
+import { Container } from '../../components/Container'
+import {
+  Actions,
+  AddressFormBox,
+  CheckoutButton,
+  CheckoutCard,
+  CheckoutFormBox,
+  CheckoutOrderFieldset,
+  CityField,
+  ComplementField,
+  DeleteButton,
+  CardDetails,
+  DistrictField,
+  FieldsWrapper,
+  FillOrderFildset,
+  FormWrapper,
+  Heading,
+  NumberField,
+  PaymentButton,
+  PaymentFormBox,
+  PaymentOptions,
+  Separator,
+  StateField,
+  StreetField,
+  ZipCodeField,
+  CartChangeAmount,
+  CheckoutInfo,
+  PaymentCheckout,
+} from './styles'
+import { useTheme } from 'styled-components'
+
 export function Checkout() {
-  return <h1>Checkout</h1>
+  const theme = useTheme()
+
+  return (
+    <main>
+      <Container>
+        <FormWrapper>
+          <FillOrderFildset>
+            <legend>Complete seu pedido</legend>
+            <AddressFormBox>
+              <Heading>
+                <MapPin size={22} color={theme['yellow-dark']} />
+                <section>
+                  <span>Endereço de Entrega</span>
+                  <small>
+                    Informe o endereço onde deseja receber seu pedido
+                  </small>
+                </section>
+              </Heading>
+
+              <FieldsWrapper>
+                <ZipCodeField type="text" placeholder="CEP" />
+                <StreetField type="text" placeholder="Rua" />
+                <NumberField type="text" placeholder="Número" />
+                <ComplementField>
+                  <input
+                    id="complement"
+                    type="text"
+                    placeholder="Complemento"
+                  />
+                  <label htmlFor="complement">Opcional</label>
+                </ComplementField>
+                <DistrictField type="text" placeholder="Bairro" />
+                <CityField type="text" placeholder="Cidade" />
+                <StateField type="text" placeholder="UF" />
+              </FieldsWrapper>
+            </AddressFormBox>
+            <PaymentFormBox>
+              <Heading>
+                <CurrencyDollar size={22} color={theme.purple} />
+                <section>
+                  <span>Pagamento</span>
+                  <small>
+                    O pagamento é feito na entrega. Escolha a forma que deseja
+                    pagar
+                  </small>
+                </section>
+              </Heading>
+              <PaymentOptions>
+                <PaymentButton>
+                  <input
+                    type="radio"
+                    name="payment-type"
+                    id="credit-card-payment"
+                    value="Cartão de Crédito"
+                  />
+                  <label htmlFor="credit-card-payment">
+                    <CreditCard size={16} color={theme.purple} />
+                    <span>Cartão de Crédito</span>
+                  </label>
+                </PaymentButton>
+                <PaymentButton>
+                  <input
+                    type="radio"
+                    name="payment-type"
+                    id="debit-card-payment"
+                    value="Cartão de Débito"
+                  />
+                  <label htmlFor="debit-card-payment">
+                    <Bank size={16} color={theme.purple} />
+                    <span>Cartão de Débito</span>
+                  </label>
+                </PaymentButton>
+                <PaymentButton>
+                  <input
+                    type="radio"
+                    name="payment-type"
+                    id="money-payment"
+                    value="Dinheiro"
+                  />
+                  <label htmlFor="money-payment">
+                    <Money size={16} color={theme.purple} />
+                    <span>Dinheiro</span>
+                  </label>
+                </PaymentButton>
+              </PaymentOptions>
+            </PaymentFormBox>
+          </FillOrderFildset>
+          <CheckoutOrderFieldset>
+            <legend>Cafés selecionados</legend>
+            <CheckoutFormBox>
+              <CheckoutCard>
+                <CheckoutInfo>
+                  <img src="/coffees/americano.png" alt="" />
+                  <CardDetails>
+                    <span>Expresso Tradicional</span>
+                    <Actions>
+                      <CartChangeAmount>
+                        <button
+                          type="button"
+                          onClick={() => console.log('clicou')}
+                        >
+                          <Minus size={14} weight="bold" />
+                        </button>
+                        <span>1</span>
+                        <button
+                          type="button"
+                          onClick={() => console.log('clicou')}
+                        >
+                          <Plus size={14} weight="bold" />
+                        </button>
+                      </CartChangeAmount>
+                      <DeleteButton type="button">
+                        <Trash size={16} color={theme.purple} />
+                        Remover
+                      </DeleteButton>
+                    </Actions>
+                  </CardDetails>
+                </CheckoutInfo>
+
+                <strong>R$ 20,00</strong>
+              </CheckoutCard>
+              <Separator />
+              <CheckoutCard>
+                <CheckoutInfo>
+                  <img src="/coffees/americano.png" alt="" />
+                  <CardDetails>
+                    <span>Expresso Tradicional</span>
+                    <Actions>
+                      <CartChangeAmount>
+                        <button
+                          type="button"
+                          onClick={() => console.log('clicou')}
+                        >
+                          <Minus size={14} weight="bold" />
+                        </button>
+                        <span>1</span>
+                        <button
+                          type="button"
+                          onClick={() => console.log('clicou')}
+                        >
+                          <Plus size={14} weight="bold" />
+                        </button>
+                      </CartChangeAmount>
+                      <DeleteButton>
+                        <Trash size={16} color={theme.purple} />
+                        Remover
+                      </DeleteButton>
+                    </Actions>
+                  </CardDetails>
+                </CheckoutInfo>
+
+                <strong>R$ 20,00</strong>
+              </CheckoutCard>
+              <Separator />
+
+              <PaymentCheckout>
+                <section>
+                  <small>Total de itens</small>
+                  <span>R$ 29,70</span>
+                </section>
+                <section>
+                  <small>Entrega</small>
+                  <span>R$ 3,50</span>
+                </section>
+                <section>
+                  <strong>Total</strong>
+                  <strong>R$ 33,20</strong>
+                </section>
+              </PaymentCheckout>
+              <CheckoutButton>Confirmar Pedido</CheckoutButton>
+            </CheckoutFormBox>
+          </CheckoutOrderFieldset>
+        </FormWrapper>
+      </Container>
+    </main>
+  )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,374 @@
+import styled from 'styled-components'
+
+export const FormWrapper = styled.form`
+  display: flex;
+  gap: 2rem;
+
+  margin-top: 2.5rem;
+`
+
+export const FieldsetBase = styled.fieldset`
+  border: 0;
+
+  & legend {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 1.125rem;
+
+    margin-bottom: 0.9375rem;
+  }
+`
+
+export const FillOrderFildset = styled(FieldsetBase)`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`
+
+export const CheckoutOrderFieldset = styled(FieldsetBase)`
+  min-width: 28rem;
+`
+
+export const FormBoxBase = styled.div`
+  background: ${(props) => props.theme['base-card']};
+
+  padding: 2.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  border-radius: 6px;
+`
+
+export const AddressFormBox = styled(FormBoxBase)`
+  gap: 2rem;
+`
+
+export const PaymentFormBox = styled(FormBoxBase)`
+  gap: 2rem;
+`
+
+export const CheckoutFormBox = styled(FormBoxBase)`
+  border-radius: 6px 44px 6px 44px;
+`
+
+export const Heading = styled.header`
+  display: flex;
+  gap: 0.5rem;
+
+  & > section {
+    display: flex;
+    flex-direction: column;
+    gap: 0.125rem;
+
+    & > span {
+      color: ${(props) => props.theme['base-subtitle']};
+    }
+
+    & > small {
+      font-size: 0.875rem;
+      color: ${(props) => props.theme['base-text']};
+    }
+  }
+`
+
+export const FieldsWrapper = styled.div`
+  display: grid;
+
+  grid-template-columns: 1fr 1fr 60px;
+  grid-template-areas:
+    'zipcode . .'
+    'street street street'
+    'number complement complement'
+    'district city state';
+
+  column-gap: 0.75rem;
+  row-gap: 1rem;
+`
+
+export const InputFieldBase = styled.input`
+  background: ${(props) => props.theme['base-input']};
+
+  padding: 0.75rem;
+
+  color: ${(props) => props.theme['base-text']};
+  font-size: 0.875rem;
+
+  border: 1px solid ${(props) => props.theme['base-button']};
+  border-radius: 4px;
+
+  &:focus {
+    box-shadow: 0 0 0 1px ${(props) => props.theme['yellow-dark']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['base-label']};
+  }
+
+  &:-webkit-autofill,
+  &:-webkit-autofill:hover,
+  &:-webkit-autofill:focus,
+  &:-webkit-autofill:active {
+    -webkit-text-fill-color: ${(props) => props.theme['base-text']};
+    -webkit-box-shadow: 0 0 0 30px ${(props) => props.theme['base-input']} inset !important;
+  }
+`
+
+export const ZipCodeField = styled(InputFieldBase)`
+  grid-area: zipcode;
+`
+export const StreetField = styled(InputFieldBase)`
+  grid-area: street;
+`
+export const NumberField = styled(InputFieldBase)`
+  grid-area: number;
+`
+
+export const ComplementField = styled.div`
+  grid-area: complement;
+
+  background: ${(props) => props.theme['base-input']};
+  border: 1px solid ${(props) => props.theme['base-button']};
+  border-radius: 4px;
+
+  padding: 0.75rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  &:focus-within {
+    box-shadow: 0 0 0 1px ${(props) => props.theme['yellow-dark']};
+
+    & label {
+      display: none;
+    }
+  }
+
+  & input {
+    background: transparent;
+
+    border: 0;
+
+    font-size: 0.875rem;
+    color: ${(props) => props.theme['base-text']};
+
+    &::placeholder {
+      color: ${(props) => props.theme['base-label']};
+    }
+
+    &:focus {
+      box-shadow: none;
+    }
+  }
+
+  & label {
+    color: ${(props) => props.theme['base-label']};
+    font-size: 0.75rem;
+    font-style: italic;
+  }
+`
+
+export const DistrictField = styled(InputFieldBase)`
+  grid-area: district;
+`
+export const CityField = styled(InputFieldBase)`
+  grid-area: city;
+`
+export const StateField = styled(InputFieldBase)`
+  grid-area: state;
+`
+
+export const PaymentOptions = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`
+
+export const PaymentButton = styled.div`
+  & label {
+    min-width: 11.166875rem;
+
+    background: ${(props) => props.theme['base-button']};
+
+    border: 1px solid transparent;
+    border-radius: 6px;
+
+    padding: 1rem;
+
+    transition: all 0.2s ease;
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    & > span {
+      font-size: 0.75rem;
+      text-transform: uppercase;
+    }
+
+    &:hover {
+      background: ${(props) => props.theme['base-hover']};
+    }
+  }
+
+  & input {
+    display: none;
+
+    &:checked ~ label {
+      background: ${(props) => props.theme['purple-light']};
+      border-color: ${(props) => props.theme.purple};
+    }
+  }
+`
+
+export const CheckoutButton = styled.button`
+  background: ${(props) => props.theme.yellow};
+
+  border: 0;
+  border-radius: 6px;
+
+  font-size: 0.875rem;
+  font-weight: bold;
+  line-height: 1.6;
+  text-transform: uppercase;
+
+  color: ${(props) => props.theme.white};
+
+  padding: 0.75rem;
+
+  transition: all 0.2s ease;
+  cursor: pointer;
+
+  &:hover {
+    background: ${(props) => props.theme['yellow-dark']};
+  }
+`
+
+export const CheckoutCard = styled.div`
+  padding: 0.5rem 0.25rem;
+
+  display: flex;
+  align-items: start;
+  justify-content: space-between;
+`
+
+export const CheckoutInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.25rem;
+
+  & img {
+    width: 4rem;
+    height: 4rem;
+  }
+`
+
+export const CardDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  & > span {
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+`
+
+export const Actions = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`
+
+export const DeleteButton = styled.button`
+  background: ${(props) => props.theme['base-button']};
+
+  font-size: 0.75rem;
+  color: ${(props) => props.theme['base-text']};
+  text-transform: uppercase;
+
+  padding: 0.5rem;
+
+  border: 0;
+  border-radius: 6px;
+
+  display: flex;
+  align-items: center;
+  gap: 0.09375rem;
+
+  transition: all 0.2s ease;
+  cursor: pointer;
+
+  &:hover {
+    background: ${(props) => props.theme['base-hover']};
+  }
+`
+
+export const Separator = styled.hr`
+  border-top: 1px solid ${(props) => props.theme['base-button']};
+
+  margin: 1.5rem 0;
+`
+
+export const CartChangeAmount = styled.div`
+  background: ${(props) => props.theme['base-button']};
+
+  padding: 0.53125rem 0.5rem;
+  border-radius: 6px;
+
+  min-height: 2.375rem;
+  min-width: 4.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 0.25rem;
+
+  & > button {
+    background: transparent;
+    border: 0;
+    line-height: 0;
+
+    cursor: pointer;
+
+    & svg {
+      fill: ${(props) => props.theme.purple};
+      transition: all 0.2s ease;
+
+      &:hover {
+        fill: ${(props) => props.theme['purple-dark']};
+      }
+    }
+  }
+
+  & > span {
+    color: ${(props) => props.theme['base-title']};
+  }
+`
+
+export const PaymentCheckout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+
+  margin-bottom: 1.5rem;
+
+  & section {
+    display: flex;
+    justify-content: space-between;
+
+    & > small {
+      font-size: 0.875rem;
+    }
+
+    & > small,
+    & > span {
+      color: ${(props) => props.theme['base-text']};
+    }
+
+    & > strong {
+      font-size: 1.25rem;
+      color: ${(props) => props.theme['base-subtitle']};
+    }
+  }
+`
