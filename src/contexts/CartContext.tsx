821ed30b8dbf74300f94fb0618ae184cb3f2c4c4
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-24 06:32:31 -0300

    feat: add cartState reducer

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,7 +1,10 @@
-import { ReactNode, createContext } from 'react'
+import { ReactNode, createContext, useEffect, useReducer } from 'react'
+import { Item, cartReducer } from '../reducers/cart/reducer'
+import { addItemToCartAction } from '../reducers/cart/actions'
 
 interface CartContextType {
-  cart: any
+  cart: Item[]
+  addItemToCart: (item: Item) => void
 }
 
 export const CartContext = createContext({} as CartContextType)
@@ -11,12 +14,41 @@ interface CartContextProviderProps {
 }
 
 export function CartContextProvider({ children }: CartContextProviderProps) {
-  const cart = 0
+  const [cartState, dispatch] = useReducer(
+    cartReducer,
+    {
+      cart: [],
+    },
+    (cartState) => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@coffee-delivery:cart-state-1.0.0',
+      )
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+
+      return cartState
+    },
+  )
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cartState)
+
+    localStorage.setItem('@coffee-delivery:cart-state-1.0.0', stateJSON)
+  }, [cartState])
+
+  const { cart } = cartState
+
+  function addItemToCart(item: Item) {
+    dispatch(addItemToCartAction(item))
+  }
 
   return (
     <CartContext.Provider
       value={{
         cart,
+        addItemToCart,
       }}
     >
       {children}
