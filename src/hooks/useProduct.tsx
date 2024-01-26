a748b10eb4e7a19357c6babc669f3305a0ea3e01
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-26 13:28:48 -0300

    feat: add useProduct custom hook

---
+++ src/hooks/useProduct.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+import { priceFormatter } from '../utils/formatter'
+import { useCart } from './useCart'
+
+import { CardProps } from '../components/Card'
+
+export function useProduct<T extends CardProps['coffee']>(coffee: T) {
+  const { addItemToCart } = useCart()
+
+  const priceFormattedWithCurrency = priceFormatter
+    .format(coffee.price)
+    .split(' ')
+  const currency = priceFormattedWithCurrency[0]
+  const price = priceFormattedWithCurrency[1]
+
+  const [cartAmount, setCartAmount] = useState(1)
+
+  function handleDecreaseCartAmount() {
+    if (cartAmount > 1) {
+      setCartAmount((prevState) => prevState - 1)
+    }
+  }
+  function handleIncreaseCartAmount() {
+    setCartAmount((prevState) => prevState + 1)
+  }
+
+  function handleAddCart() {
+    const id = coffee.id
+    const amount = cartAmount
+
+    addItemToCart({ id, amount })
+    setCartAmount(1)
+  }
+
+  return {
+    currency,
+    price,
+    cartAmount,
+    handleDecreaseCartAmount,
+    handleIncreaseCartAmount,
+    handleAddCart,
+  }
+}
