64f87e15401bb2d5461dd86e2adc4cd913a3dd6f
Michael Matheus <michaelmatheus2001@gmail.com>
2024-01-26 13:28:21 -0300

    feat: add product card actions

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,4 @@
 import { Minus, Plus, ShoppingCart } from '@phosphor-icons/react'
-import { priceFormatter } from '../../utils/formatter'
 import {
   Actions,
   CardWrapper,
@@ -11,7 +10,9 @@ import {
   Tags,
 } from './styles'
 
-interface CardProps {
+import { useProduct } from '../../hooks/useProduct'
+
+export interface CardProps {
   coffee: {
     id: number
     image: string
@@ -23,11 +24,14 @@ interface CardProps {
 }
 
 export function Card({ coffee }: CardProps) {
-  const priceFormattedWithCurrency = priceFormatter
-    .format(coffee.price)
-    .split(' ')
-  const currency = priceFormattedWithCurrency[0]
-  const price = priceFormattedWithCurrency[1]
+  const {
+    currency,
+    price,
+    cartAmount,
+    handleAddCart,
+    handleDecreaseCartAmount,
+    handleIncreaseCartAmount,
+  } = useProduct(coffee)
 
   return (
     <CardWrapper>
@@ -52,15 +56,15 @@ export function Card({ coffee }: CardProps) {
         </Price>
         <Actions>
           <CartDispatchAmount>
-            <button>
+            <button type="button" onClick={handleDecreaseCartAmount}>
               <Minus size={14} weight="bold" />
             </button>
-            <span>1</span>
-            <button>
+            <span>{cartAmount}</span>
+            <button type="button" onClick={handleIncreaseCartAmount}>
               <Plus size={14} weight="bold" />
             </button>
           </CartDispatchAmount>
-          <CartDispatchButton type="button">
+          <CartDispatchButton type="button" onClick={handleAddCart}>
             <ShoppingCart size={22} weight="fill" />
           </CartDispatchButton>
         </Actions>
