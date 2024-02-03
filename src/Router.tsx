a150504102d1b34616d7812754be44f4bc626d5a
Michael Matheus <michaelmatheus2001@gmail.com>
2024-02-03 16:09:22 -0300

    feat: add redirect to home if card data is empty

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/Home'
 import { Checkout } from './pages/Checkout'
 import { Success } from './pages/Success'
 
+import { useCheckout } from './hooks/useCheckout'
+
 export function Router() {
+  const { itemsOnCart } = useCheckout()
+
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route
+          path="/checkout"
+          element={itemsOnCart.length < 1 ? <Navigate to="/" /> : <Checkout />}
+        />
         <Route path="/success" element={<Success />} />
         <Route path="*" element={<Home />} />
       </Route>
